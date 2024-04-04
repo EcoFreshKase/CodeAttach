cf3a79b53ab086b1146504bc4cfdd15552099f00
EcoFreshKase <alexander.jablo@gmail.com>
2024-04-04 13:57:12 +0200

    fix formatting

--- src/utils/htmlParser.ts
+++ src/utils/htmlParser.ts
@@ -1,7 +1,5 @@
 export function embedInHtml(filePath: string) {
-  return `
-    <body style="margin: 0px">
-    <img src="${filePath}" />
-    </body>
-    `;
+  return `<body style="margin: 0px">
+<img src="${filePath}" />
+</body>`;
 }
